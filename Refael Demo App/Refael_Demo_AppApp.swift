53d6db88b6533b0a290bc57486a3f041eac4d9b0
refael iliaguyev <refael@gthsoftware.com>
2022-12-28 11:41:47 +0200

    Initial Commit

---
+++ Refael Demo App/Refael_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Refael_Demo_AppApp.swift
+//  Refael Demo App
+//
+//  Created by refael iliaguyev on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Refael_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
